bd1e71f8c6fd843f1e2086c1869b88189f88607e
Wahyu Setianto <wahyusetianto19@gmail.com>
2023-05-25 00:02:32 +0700

    Prepare to deploy in GH pages and add model downloader percentage

--- yolo-nas-web/src/App.js
+++ yolo-nas-web/src/App.js
@@ -3,17 +3,18 @@ import cv from "@techstark/opencv-js";
 import { Tensor, InferenceSession } from "onnxruntime-web";
 import Loader from "./components/loader";
 import { detectImage } from "./utils/detect";
+import { download } from "./utils/download";
 import "./style/App.css";
 
 const App = () => {
   const [session, setSession] = useState(null);
-  const [loading, setLoading] = useState("Loading OpenCV.js...");
+  const [loading, setLoading] = useState({ text: "Loading OpenCV.js", progress: null });
   const [image, setImage] = useState(null);
   const inputImage = useRef(null);
   const imageRef = useRef(null);
   const canvasRef = useRef(null);
 
-  // Configs
+  // configs
   const modelName = "<YOLO-NAS-MODELS>.onnx";
   const modelInputShape = [1, 3, 640, 640];
   const topk = 100;
@@ -22,37 +23,48 @@ const App = () => {
 
   // wait until opencv.js initialized
   cv["onRuntimeInitialized"] = async () => {
+    const baseModelURL = `${process.env.PUBLIC_URL}/model`;
+
     // create session
-    setLoading("Loading YOLO-NAS model...");
-    const [yoloNAS, nms] = await Promise.all([
-      InferenceSession.create(`${process.env.PUBLIC_URL}/model/${modelName}`),
-      InferenceSession.create(`${process.env.PUBLIC_URL}/model/nms-yolo-nas.onnx`),
-    ]);
+    const arrBufNet = await download(
+      `${baseModelURL}/${modelName}`, // url
+      ["Loading YOLO-NAS model", setLoading] // logger
+    ); // get model arraybuffer
+    const yoloNAS = await InferenceSession.create(arrBufNet);
+    const arrBufNMS = await download(
+      `${baseModelURL}/nms-yolo-nas.onnx`, // url
+      ["Loading NMS model", setLoading] // logger
+    ); // get nms model arraybuffer
+    const nms = await InferenceSession.create(arrBufNMS);
 
     // warmup main model
-    setLoading("Warming up model...");
+    setLoading({ text: "Warming up model...", progress: null });
     const tensor = new Tensor(
       "float32",
       new Float32Array(modelInputShape.reduce((a, b) => a * b)),
       modelInputShape
     );
     await yoloNAS.run({ "input.1": tensor });
-  
+
     setSession({ net: yoloNAS, nms: nms });
     setLoading(null);
   };
 
   return (
     <div className="App">
-      {loading && <Loader>{loading}</Loader>}
+      {loading && (
+        <Loader>
+          {loading.progress ? `${loading.text} - ${loading.progress}%` : loading.text}
+        </Loader>
+      )}
       <div className="header">
         <h1>YOLO-NAS Object Detection App</h1>
         <p>
           YOLO-NAS object detection application live on browser powered by{" "}
-          <code>onnxruntime-web</code>
+          <strong>onnxruntime-web</strong>
         </p>
         <p>
-          Serving : <code className="code">{modelName}</code>
+          Serving : <code>{modelName}</code>
         </p>
       </div>
 
--- yolo-nas-web/src/index.js
+++ yolo-nas-web/src/index.js
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState, Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import Loader from "./components/loader";
 import "./style/index.css";
+
+const onDev = false;
+const App = lazy(() => import("./App"));
+
+const Main = () => {
+  const [show, setShow] = useState(onDev);
+
+  return (
+    <>
+      {!show && (
+        <div className="agreement-con">
+          <div className="agreement">
+            <h2>⚠️ YOLO-NAS-WEB : Big size webapp!</h2>
+            <p>
+              Need big amount of data to load application and YOLO-NAS model. Roughly about{" "}
+              <strong>61.3 MB</strong> data to load this app, Do you want to proceed?
+            </p>
+          </div>
+          <button
+            onClick={() => {
+              setShow(true);
+            }}
+          >
+            Yes, I agree
+          </button>
+        </div>
+      )}
+      <hr />
+      {show && (
+        <Suspense fallback={<Loader>Load main component...</Loader>}>
+          <App />
+        </Suspense>
+      )}
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
